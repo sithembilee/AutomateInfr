4174dff93fcdd9ccf0b22f4e8250ad6d8d662d64
sithembilee <46643559+sithembilee@users.noreply.github.com>
2025-08-02 22:11:05 +0200

    Create index.js
    adding code

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
